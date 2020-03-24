53bcfe47f3432de6a8e66e1cacaa90833715583a
cy360124 <cy360124@163.com>
2020-03-24 21:25:02 +0800

    change "message" in account.view

--- account/views.py
+++ account/views.py
@@ -7,69 +7,78 @@ from .models import User
 
 def register(request):
     ''' 实现用户注册功能，须以POST方式
+
+    Arguments:
+       request: It should contains {"username":<str>, "password":<str>} in the body.
     
     Return:
-        HttpRepsonse, which contains {'status':<int>, 'description':<str>} as below
-        0 --> 注册成功
-        1 --> 该用户名已存在
-        2 --> 用户名或密码为空
-        3 --> 请求方式错误
+        HttpRepsonse, which contains {"err_code":<int>, "message":<str>} as below
+         0 --> 注册成功
+        -1 --> 注册失败, message是相关错误信息
     '''
 
-    if request.method == 'POST':
+    if request.method == "POST":
         content = json.loads(request.body)
-        username = content.get('username')
-        password = content.get('password')
+        username = content.get("username")
+        password = content.get("password")
         if username == "" or password == "":
-            content = {'status':2, 'description':'用户名或密码为空'}
+            content = {"err_code":-1, "message":"用户名或密码为空"}
         elif User.objects.filter(username=username).exists():
-            content = {'status':1, 'description':'该用户名已存在'}
+            content = {"err_code":-1, "message":"该用户名已存在"}
         else:
             User.objects.create(username=username, password=password)
-            content = {'status':0, 'description':'注册成功'}
+            content = {"err_code":0, "message":"注册成功"}
     else:
-        content = {'status':3, 'description':'请求方式错误'}
+        content = {"err_code":-1, "message":"请求方式错误"}
 
     return HttpResponse(json.dumps(content))
     
 
 def login(request):
-    ''' 实现登录功能，须以POST方式
+    ''' 实现登录功能，须以POST方式. If successful, cookie would be set.
+
+    Arguments:
+       request: It should contains {"username":<str>, "password":<str>} in the body.
     
     Return:
-        HttpRepsonse, which contains {'status':<int>, 'description':<str>} as below
-        0 --> 登录成功
-        1 --> 用户名或密码错误
-        2 --> 请求方式错误
+        HttpRepsonse, which contains {"err_code":<int>, "message":<str>} as below
+         0 --> 登录成功
+        -1 --> 登录失败, message是相关错误信息
     '''
 
-    if request.method == 'POST':
+    if request.method == "POST":
         content = json.loads(request.body)
-        username = content.get('username')
-        password = content.get('password')
+        username = content.get("username")
+        password = content.get("password")
         if User.objects.filter(username=username, password=password).exists():
             user = User.objects.get(username=username, password=password)
             cookie_value = make_cookie()
             user.cookie_value = cookie_value
             user.save()
-            content = {'status':0, 'description':'登录成功'}
+            content = {"err_code":0, "message":"登录成功"}
             response = HttpResponse(json.dumps(content))
-            response.set_cookie('cookie_value', cookie_value, max_age=2*60*60)  # 设置cookie，有效时间2小时
+            response.set_cookie("cookie_value", cookie_value, max_age=6*60*60)  # 设置cookie，有效时间6小时
             return response
         else:
-            content = {'status':1, 'description':'用户名或密码错误'}
+            content = {"err_code":-1, "message":"用户名或密码错误"}
             return HttpResponse(json.dumps(content))
     else:
-        content = {'status':2, 'description':'请求方式错误'}
+        content = {"err_code":-1, "message":"请求方式错误"}
         return HttpResponse(json.dumps(content))
 
 
 def logout(request):
     ''' 退出登录
+
+    Arguments:
+        request: no data in the body
+    
+    Return:
+        A HtttpResponse, whose cookie is deleted.
     '''
     
     response = HttpResponse()
-    response.delete_cookie('cookie_value')
+    response.delete_cookie("cookie_value")
     return response
 
 
@@ -85,7 +94,7 @@ def make_cookie(length = 50):
         生成的随机字符串，即cookie value
     '''
 
-    cookie_value = ''
+    cookie_value = ""
     for i in range(length):
         cookie_value += str(random.randint(0, 9))
     
@@ -99,7 +108,7 @@ def is_login(request):
         True: 已登录；  False: 未登录
     '''
 
-    cookie_value = request.COOKIES.get('cookie_value')
+    cookie_value = request.COOKIES.get("cookie_value")
     if not cookie_value:
         return False
     elif not User.objects.filter(cookie_value=cookie_value).exists():
@@ -116,7 +125,7 @@ def ask_user(request):
         未登录时，返回None
     '''
 
-    cookie_value = request.COOKIES.get('cookie_value')
+    cookie_value = request.COOKIES.get("cookie_value")
     if not cookie_value:
         return None
     else:
