6e0c4c90bf381da3e352b57c132e733c98195eb5
cy360124 <cy360124@163.com>
2020-05-01 15:10:42 +0800

    implement the API of Answer

--- issue/views.py
+++ issue/views.py
@@ -6,7 +6,7 @@ from .models import Issue, Answer
 
 # Create your views here.
 
-def create(request):
+def issue_create(request):
     ''' 创建一个新Issue
     '''
     if request.method == "POST":
@@ -27,7 +27,7 @@ def create(request):
     return HttpResponse(json.dumps(response_content))
 
 
-def delete(request, issue_id):
+def issue_delete(request, issue_id):
     ''' 删除某个Issue
     '''
     user = backend_ask_login_user(request)
@@ -47,7 +47,7 @@ def delete(request, issue_id):
     return HttpResponse(json.dumps(response_content))
 
 
-def detail(request, issue_id):
+def issue_detail(request, issue_id):
     ''' 显示某个Issue的详细内容
     '''
     try:
@@ -61,7 +61,7 @@ def detail(request, issue_id):
     return HttpResponse(json.dumps(response_content))
 
 
-def search(request):
+def issue_search(request):
     ''' 搜寻Issue
     '''
     if request.method == "POST":
@@ -79,7 +79,7 @@ def search(request):
     return HttpResponse(json.dumps(response_content))
 
 
-def collect(request, issue_id):
+def issue_collect(request, issue_id):
     '''收藏或取消收藏某个Issue
     '''
     user = backend_ask_login_user(request)
@@ -100,7 +100,7 @@ def collect(request, issue_id):
     return HttpResponse(json.dumps(response_content))
 
 
-def collection_list(request):
+def issue_collection_list(request):
     ''' 获取登录用户的收藏列表
     '''
     user = backend_ask_login_user(request)
@@ -136,6 +136,98 @@ def issue_like(request, issue_id):
 
     return HttpResponse(json.dumps(response_content))
 
+
+def answer_create(request, issue_id):
+    ''' 对某个Issue新建一个回答
+    '''
+    if request.method == "POST":
+        data = json.loads(request.body)
+        content = data.get("content")
+        user = backend_ask_login_user(request)
+        if not user:
+            response_content = {"err_code":-1, "message":"当前未登录", "data":None}
+        else:
+            try:
+                issue = Issue.objects.get(id=issue_id)
+                Answer.objects.create(issue=issue, replier=user, pub_date=timezone.now(), content=content)
+                response_content = {"err_code":0, "message":"回答已发布", "data":None}
+            except Issue.DoesNotExist:
+                response_content = {"err_code":-1, "message":"该问题不存在", "data":None}
+    else:
+        response_content = {"err_code":-1, "message":"请求方式错误", "data":None}
+    
+    return HttpResponse(json.dumps(response_content))
+
+
+def answer_delete(request, answer_id):
+    ''' 删除某个answer
+    '''
+    user = backend_ask_login_user(request)
+    if not user:
+        response_content = {"err_code":-1, "message":"当前未登录", "data":None}
+    else:
+        try:
+            answer = Answer.objects.get(id=answer_id)
+            if answer.replier == user:
+                answer.delete()
+                response_content = {"err_code":0, "message":"回答已删除", "data":None}
+            else:
+                response_content = {"err_code":-1, "message":"您不是该回答的发布者，无法删除", "data":None}
+        except Answer.DoesNotExist:
+            response_content = {"err_code":-1, "message":"该回答不存在", "data":None}
+    
+    return HttpResponse(json.dumps(response_content))
+
+
+def answer_detail(request, answer_id):
+    ''' 获取某个Answer的详细数据
+    '''
+    try:
+        answer = Answer.objects.get(id=answer_id)
+        user = backend_ask_login_user(request)
+        obj = conduct_detail_answer(answer, user)
+        response_content = {"err_code":0, "message":"查询成功", "data":obj}
+    except Answer.DoesNotExist:
+        response_content = {"err_code":-1, "message":"该回答不存在", "data":None}
+    
+    return HttpResponse(json.dumps(response_content))
+
+
+def answer_list(request, issue_id):
+    ''' 获取某个Issue的回答列表
+    '''
+    try:
+        issue = Issue.objects.get(id=issue_id)
+        user = backend_ask_login_user(request)
+        obj_list = []
+        for answer in issue.answer_set.all().order_by("pub_date"):
+            obj = conduct_detail_answer(answer, user)
+            obj_list.append(obj)
+        response_content = {"err_code":0, "message":"查询成功", "data":obj_list}
+    except Issue.DoesNotExist:
+        response_content = {"err_code":-1, "message":"该问题不存在", "data":None}
+    
+    return HttpResponse(json.dumps(response_content))
+
+
+def answer_like(request, answer_id):
+    ''' 对某个回答点赞/取消点赞
+    '''
+    try:
+        answer = Answer.objects.get(id=answer_id)
+        user = backend_ask_login_user(request)
+        if not user:
+            response_content = {"err_code":-1, "message":"当前未登录", "data":None}
+        elif len(answer.likers.filter(id=user.id)) == 0 :
+            answer.likers.add(user)
+            response_content = {"err_code":0, "message":"点赞成功", "data":None}
+        else:
+            answer.likers.remove(user)
+            response_content = {"err_code":0, "message":"已取消点赞", "data":None}
+    except Answer.DoesNotExist:
+        response_content = {"err_code":-1, "message":"该回答不存在", "data":None}
+    
+    return HttpResponse(json.dumps(response_content))
 
 
 # some assist functions
@@ -187,3 +279,21 @@ def conduct_brief_issue(issue):
     '''
     return {"id":issue.id, "type":issue.Type, "title":issue.title, "author":issue.author.username, "pub_date":str(issue.pub_date)[0:16], \
             "collect_num":issue.collectors.count(), "like_num":issue.likers.count()}
+
+
+def conduct_detail_answer(answer, user):
+    ''' 构造详细版Answer实例
+    '''
+    ID = answer.id
+    author = answer.replier.username
+    pub_date = str(answer.pub_date)[0:16]
+    content = answer.content
+    like_num = answer.likers.count()
+    if not user:
+        IsLiking = False
+    else:
+        IsLiking = not (len(answer.likers.filter(id=user.id)) == 0)
+    comment_num = 0
+
+    return {"id":ID, "author":author, "pub_date":pub_date, "content":content, \
+            "like_num":like_num, "IsLiking":IsLiking, "comment_num":comment_num}
